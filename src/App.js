c08948953f0e24401b59a31cc389470f815650fc
Victor <sakhnovict@gmail.com>
2020-11-11 14:55:54 +0700

    C:/Program Files/Git/login - loginPage компонент

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Login from './LoginPage/Login';
+
+export default function App() {
   return (
-    <div>
-      <h1>Simple Page Development</h1>
-    </div>
-  )
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
