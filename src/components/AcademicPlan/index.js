21abf908f75a8b4300638f99f57e43aadb7aeb97
Andrey <agazzaev88@gmail.com>
2021-03-17 20:18:12 +0700

    public компоненты

--- src/components/AcademicPlan/index.js
+++ src/components/AcademicPlan/index.js
@@ -1,9 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import './index.scss'
 import '../../styles/reprise.scss'
 
 import { Header } from '../Header';
+
+import { Semester, Searcher } from '../Public';
 
 export const AcademicPlan = () => {
     return (
@@ -12,9 +14,12 @@ export const AcademicPlan = () => {
             <h1 className="main-title">
                 <a className="main-title__link">Учебный план</a>
             </h1>
-
+            <div className="filter">
+                <Searcher />
+                <Semester />
+            </div>
             <section className="academic-plan container">
-                <ListOfOrdersFragments pageWidth={document.documentElement.scrollWidth} />
+                {/* <ListOfOrdersFragments pageWidth={document.documentElement.scrollWidth} /> */}
             </section>
         </>
     )
