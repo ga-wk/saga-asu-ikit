047c03680daf6d7aff6542c199b52b2846e64274
Andrey <agazzaev88@gmail.com>
2021-05-12 20:16:55 +0700

    the structure of the questionnaire was created

--- src/components/Questionnaire/index.js
+++ src/components/Questionnaire/index.js
@@ -2,10 +2,58 @@ import React from "react";
 
 import "./index.scss";
 
-export const Questionnaire = ({ id }) => {
+const RenderQuestions = ({ questions, parIndex }) => {
+  return questions.map((el, index) => {
+    return (
+      <li className="questions__item" key={index + "_" + parIndex}>
+        <p className="questions__item-title">{el.name}</p>
+        {el.answers.map((ans) => {
+          return (
+            <label className="questions__item-question" key={index + "_" + parIndex + ans.name}>
+              <input type="radio" name={el.name} />
+              {ans.name}
+              {el.type === 2 || ans.custom ? (
+                <input type="text" placeholder="Комментарий" />
+              ) : null}
+            </label>
+          );
+        })}
+
+        {el.type === 0 ? (
+          <label className="questions__item-question">
+            <input type="radio" name={el.name} />
+            Затрудняюсь ответить
+          </label>
+        ) : null}
+      </li>
+    );
+  });
+};
+
+const RenderBlocks = ({ blocks }) => {
+  return blocks.map((el, index) => {
+    return (
+      <li className="blocks__item" key={index}>
+        <p className="blocks__item-title" >{el.name}</p>
+        <ul className ="questions__list">
+          <RenderQuestions questions={el.questions} parIndex={index} />
+        </ul>
+      </li>
+    );
+  });
+};
+
+export const Questionnaire = ({ questionnaire }) => {
+  console.log(questionnaire.questionnaire.blocks);
   return (
-    <div>
-      <p> Анкета №{id} </p>
+    <div className="questionnaire container">
+      <form className="questionnaire__form">
+        <ul className="blocks__list">
+          <RenderBlocks blocks={questionnaire.questionnaire.blocks} />
+        </ul>
+
+        <button className="questionnaire__submit" type="submit"> Отправить ответ </button>
+      </form>
     </div>
   );
 };
--- src/pages/QuestionnairePage.js
+++ src/pages/QuestionnairePage.js
@@ -6,44 +6,72 @@ import { Header } from "../components/Header";
 import { Cookie } from "../libs/cookie.js";
 import { postData } from "../libs/requests";
 import { login } from "../strings/links";
-import { studentGet } from "../strings/urls";
+import { questionnaireOne, studentGet } from "../strings/urls";
 import { token } from "../strings/public";
 
 export const QuestionnairePage = (props) => {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [user, setUser] = useState(false);
-
-  const data = {
+  const [questionnaire, setQuestionnaire] = useState(false);
+  const studentData = {
     text: "",
     userToken: Cookie.getCookie(token),
   };
 
+  const questionnaireData = {
+    userToken: Cookie.getCookie(token),
+    id: props.match.params.id,
+  };
+
   useEffect(() => {
-    const promiseStudent = postData(studentGet, data);
+    const promiseStudent = postData(studentGet, studentData);
     if (promiseStudent !== undefined) {
       promiseStudent
         .then((res) => res.json())
         .then(
           (result) => {
             setUser(result);
-            setIsLoaded(true);
           },
           (error) => {
             setError(error);
-            setIsLoaded(true);
           }
         );
     } else {
       setIsLoaded(false);
     }
+
+    const promiseQuestionnaire = postData(questionnaireOne, questionnaireData);
+    if (promiseQuestionnaire !== undefined) {
+      promiseQuestionnaire
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            setQuestionnaire(result);
+          },
+          (error) => {
+            setError(error);
+          }
+        );
+    } else {
+      setIsLoaded(false);
+    }
+
+    Promise.all([promiseStudent, promiseQuestionnaire])
+      .then(() => {
+        setIsLoaded(true);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoaded(true);
+      });
   }, [isLoaded]);
 
   if (!Cookie.getCookie(token)) return <Redirect to={login} />;
 
   if (error) {
     return <div>Ошибка: {error.message}</div>;
-  } else if (!isLoaded || !user) {
+  } else if (!isLoaded || !user || !questionnaire) {
     return <div>Загрузка...</div>;
   } else {
     return (
@@ -52,7 +80,7 @@ export const QuestionnairePage = (props) => {
           firstName={`${user.student.surname}`}
           fullName={`${user.student.surname} ${user.student.name[0]}. ${user.student.patronymic[0]}.`}
         />
-        <Questionnaire id={props.match.params.id} />
+        <Questionnaire questionnaire={questionnaire} />
       </Fragment>
     );
   }
